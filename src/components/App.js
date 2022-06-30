dc2652331c36d2666beef1a92ba9a219b7d7a33f
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-06-30 21:01:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,106 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const flamesArr = ["Siblings","Friends","Love","Affection","Marriage","Enemy"];
-function App () {
-    firstName = '';
-    secondName = '';
-    const[output,setOutput] = useState('No Relationship');
+export default function App() {
 
-    function changeHandler1 (event) {
-        firstName = event.target.value;
-    }
+const [str1Value,setVal1] = useState("");
+const [str2Value,setVal2] = useState("");
+ 
+const [dis,setDis] = useState("");
 
-    function changeHandler2 (event) {
-        secondName = event.target.value;
-    }
-
-    function mergeString(firstInput, secondInput) {
-        let firstMap = {};
-        let secondMap = {};
-        for(let i=0; i<firstInput; i++) {
-            if(!firstMap[firstInput[i]])
-                firstMap[firstInput[i]] = 0;
-                firstMap[firstInput[i]] += 1;
-        }
-
-        for(let i=0; i<secondInput; i++) {
-            if(!secondMap[secondInput[i]]) 
-                secondMap[secondInput[i]] = 0;
-                secondMap[secondInput[i]] += 1;
-            
-        }
-
-        let count = 0;
-        for(let first in firstMap) {
-            for(let second in secondMap) {
-                if(firstMap[first] !=0 && secondMap[first] !=0) {
-                    if(secondMap[first]) {
-                        firstMap[first]--;
-                        secondMap[first]--;
-                    }
-                }
-            }
-        }
-
-        for(let item in firstMap) {
-            count += firstMap[item];
-        }
-
-        for(let item in secondMap) {
-            count += secondMap[item];
-        }
-
-        return count%6;
-    }
-
-    function calculate () {
-        if(document.querySelector('#input1').value=0 || document.querySelector('#input2').value==0)
-            aler('Please Enter valid input')
-        let finalOutput = mergeString(firstName.toLowerCase(), secondName.toLowerCase());
-        setOutput(flamesArr[finalOutput]);
-    }
-
-    function clear () {
-        firstName = '';
-        secondName ='';
-    }
-     return(
-            <div id="main">
-               {/* Do not remove the main div */}
-               <h3 data-testid="answer">{output}</h3>
-               <label>First Name:</label>
-               <input type="text" data-testid="input1" onChange={changeHandler1}/>
-               <label>second Name:</label>
-               <input type="text" data-testid="input2" onChange={changeHandler2}/>
-               <button data-testid="calculate_relationship"  onClick={calculate}>Calculate Relationship Future</button>
-               <button data-testid="clear" onClick={clear}>Click</button>
-            </div>
-        )
+function str1Fun (e){
+setVal1(e.target.value);
 }
 
+function str2Fun (e){
+  setVal2(e.target.value);
+}
 
-export default App;
+function calculate () {
+ let map1 = {};
+ let map2 = {};
+}
+
+function clear (e) {
+  setDis("");
+  document.getElementById("int1").value ="";
+  document.getElementById("int2").value ="";
+}
+
+function mergeInput(f,l) {
+  let map1 = {};
+  let map2 = {};
+    for(let i=0;i<f.length;i++) {
+
+      if(!map1[f[i]]) {
+        map1[f[i]] = 1;
+      }
+
+      else{
+        map1[f[i]]++;
+      }
+    }
+
+    for(let i=0;i<l.length;i++) {
+      if(!map2[l[i]]) {
+        map2[l[i]] = 1;
+      }
+      else{
+        map2[l[i]]++;
+      }
+    }
+    let count = 0;
+    for(let i in map1) {
+      for(let j in map2) {
+        if(map1[i]!=0 && map2[i]) {
+          if(map2[i]) {
+            map1[i]--;
+            map2[i]--;
+          }
+        }
+      }
+    }
+    for(let i in map1) {
+      count+=map1[i];
+    }
+
+    for(let i in map2) {
+      count+= map2[i];
+    }
+
+    return count%6;
 
 
+}
+
+const flames = ["Siblings","Friends","Love","Affection","Marriage","Enemy"];
+function calculate () {
+ 
+    if(str1Value.length===0 || str2Value.length===0) {
+      setDis("Please Enter valid input");
+    } 
+     
+      else{
+        const FinalResult = mergeInput(str1Value.toLowerCase(),str2Value.toLowerCase());
+        setDis(flames[FinalResult]); 
+     }
+     
+  
+}
+
+  return (
+    <div id="main">
+    <h3 data-testid="answer">{dis}</h3>
+    <div className="input">
+    <input data-testid="input1" id="int1" required onChange={str1Fun}/>
+    <input data-testid="input2" id="int2" required onChange={str2Fun}/>
+    </div>
+  
+   <div className="buttons">
+    
+   <button data-testid="calculate_relationship" onClick={calculate}>Calculate Relationship Future</button>
+   <button data-testid="clear" onClick={clear} >clear</button>
+   </div>
+</div>
+  )
+}
